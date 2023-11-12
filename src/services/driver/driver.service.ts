331a528f551e7b7fe7441e9221f67d1b8e8482be
Roger Vargas <roger.vargas@cwi.com.br>
2023-11-12 08:41:00 -0300

    feat: add listDrivers findDriver isDriverExist

--- src/services/driver/driver.service.ts
+++ src/services/driver/driver.service.ts
@@ -1,10 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ListDriversDto } from 'src/dtos/driver/listDrivers.dto';
 import { Driver } from 'src/entities/driver/driver.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class DriverService {
-  createDriver(name: string, cpf: string) {
-    const newDriver = new Driver(name, cpf);
-    return newDriver;
+  constructor(
+    @InjectRepository(Driver)
+    private readonly driverRepository: Repository<Driver>,
+  ) {}
+
+  async createDriver(name: string, cpf: string) {
+    const driver = new Driver(name, cpf);
+    await this.driverRepository.save(driver);
+  }
+
+  async listAllDrivers(): Promise<ListDriversDto[]> {
+    const registeredDrivers = await this.driverRepository.find();
+    const driversList = registeredDrivers.map(
+      (driver) =>
+        new ListDriversDto(driver.id, driver.name, driver.cpf, driver.supplies),
+    );
+
+    return driversList;
+  }
+
+  async findDriverByCpf(cpf: string): Promise<Driver> {
+    return await this.driverRepository.findOne({
+      where: { cpf },
+    });
+  }
+
+  async isDriverExist(cpf: string): Promise<boolean> {
+    return await this.driverRepository.exist({
+      where: { cpf },
+    });
   }
 }
